3c97eb5a0cf77d1cf293ee8edd7b6fe335af351f
Tilksewm <tilksewmisganaw01@gmail.com>
2023-08-06 23:29:10 -0900

    theird commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 using namespace std;
-
+class complex{
+public:
+	string name;
+	void print(){
+	cout<<name<<endl;
+	}
+};
 int main()
 {
+	complex com;
+	com.name="tilksew";
+	com.print();
 
     cout  << endl;
     return 0;
